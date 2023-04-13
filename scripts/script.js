4473b91f60553fb8d749de6d2fe01119e14a917a
Daniil Rysin <rysin.000@gmail.com>
2023-04-13 17:26:14 +0300

    first try

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+let editBtn = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closeBtn = document.querySelector(".popup__close-btn");
+let inputName = document.querySelector(".input-name");
+let inputDescription = document.querySelector(".input-description");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let editForm = document.querySelector(".popup__form");
+
+editBtn.addEventListener("click", function () {
+    popup.classList.add("popup_opened");
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
+})
+
+closeBtn.addEventListener("click", function () {
+    popup.classList.remove("popup_opened");
+})
+
+editForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    popup.classList.remove("popup_opened");
+})
